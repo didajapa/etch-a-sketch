c09f0304c83eef550bc36f110434a06a63340709
didajapa <116636770+didajapa@users.noreply.github.com>
2023-01-12 15:02:21 +0100

    Add slider

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const container = document.getElementsByClassName('gridContainer');
 
+const slider = document.getElementsByClassName('gridSizeSlider');
+const size = document.getElementById('gridSize');
+size.innerHTML = slider.value;
+console.log(size.innerHTML);
+
+slider.oninput = function(){
+    size.innerHTML = this.value;
+    console.log(this.value);
+}
+
 makeDivs(16);
 
+// Functions
 function makeDivs(rowColumnDivs) {
     let numberOfDivs = rowColumnDivs * rowColumnDivs;
     
     for (let i = 0; i < numberOfDivs; i++){
     const newDiv = document.createElement("div");
     newDiv.setAttribute("class", "innerDiv");
-    console.log(newDiv);
+    // console.log(newDiv);
     container[0].appendChild(newDiv);
     };
 }
