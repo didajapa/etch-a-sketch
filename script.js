83a74690c4e88b3ec16a6c4130bda993c0765db9
didajapa <116636770+didajapa@users.noreply.github.com>
2023-01-12 14:08:33 +0100

    Clean-up js

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
 const container = document.getElementsByClassName('gridContainer');
 
-// function makeGrid(rows, cols) {
-//     // container.style.setProperty("--grid-rows", rows);
-//     // container.style.setProperty("--grid-cols", cols);
-//     for (i = 0; 1 < (rows * cols); i++) {
-//         let newDiv = document.createElement("div");
-//         newDiv.innerText = (i + 1);
-//         // newDiv.setAttribute("class", "innerDiv"); 
-//         container[0].appendChild(newDiv).className = "innerDiv";
-//     };
-// };
+makeDivs(16);
 
-// makeGrid(16, 16);
-
-
-makeDivs(16*16);
-
-function makeDivs(numberOfDivs) {
+function makeDivs(rowColumnDivs) {
+    let numberOfDivs = rowColumnDivs * rowColumnDivs;
+    
     for (let i = 0; i < numberOfDivs; i++){
     const newDiv = document.createElement("div");
-    // newDiv.innerText = (i + 1);
     newDiv.setAttribute("class", "innerDiv");
     console.log(newDiv);
     container[0].appendChild(newDiv);
