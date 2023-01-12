6c2911dfe5e012862eb2e6f2f080cd510902b6a5
didajapa <116636770+didajapa@users.noreply.github.com>
2023-01-12 21:54:04 +0100

    Update grid with slider

--- script.js
+++ script.js
@@ -1,27 +1,55 @@
-const container = document.getElementsByClassName('gridContainer');
+const DEFAULT_SIZE = 16;
+let currentSize = DEFAULT_SIZE;
 
-const slider = document.getElementsByClassName('gridSizeSlider');
-const size = document.getElementById('gridSize');
-size.innerHTML = slider.value;
-console.log(size.innerHTML);
+const gridSizeSlider = document.getElementById('gridSizeSlider');
+const gridSize = document.getElementById('gridSize');
+const container = document.getElementById('gridContainer');
 
-slider.oninput = function(){
-    size.innerHTML = this.value;
-    console.log(this.value);
+
+// gridSizeSlider.onmousemove = (e) => updateSizeValue(e.target.value);
+gridSizeSlider.onchange = (e) => changeSize(e.target.value);
+
+
+
+function updateSizeValue(value) {
+    gridSize.innerHTML = `${value} x ${value}`;
 }
 
-makeDivs(16);
-
-// Functions
-function makeDivs(rowColumnDivs) {
-    let numberOfDivs = rowColumnDivs * rowColumnDivs;
-    
-    for (let i = 0; i < numberOfDivs; i++){
-    const newDiv = document.createElement("div");
-    newDiv.setAttribute("class", "innerDiv");
-    // console.log(newDiv);
-    container[0].appendChild(newDiv);
-    };
+function changeSize(value) {
+    setCurrentSize(value);
+    updateSizeValue(value);
+    reloadGrid();
 }
 
+function setCurrentSize(newSize) {
+    currentSize = newSize;
+}
 
+function reloadGrid() {
+    clearGrid();
+    makeGrid(currentSize);
+}
+
+function clearGrid() {
+    console.log(currentSize);
+    container.innerHTML = '';
+}
+
+function makeGrid(size) {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+    for (let i = 0; i < size * size; i++) {
+        const innerDiv = document.createElement('div');
+        innerDiv.classList.add('innerDiv');
+        // innerDiv.addEventListener('mouseover')
+        container.appendChild(innerDiv);
+    }
+    gridSize.innerHTML = `${size} x ${size}`;
+
+
+}
+
+window.onload = () => {
+    makeGrid(DEFAULT_SIZE);
+}
